d822bafb8bd9936885a67aa0bffafe9c5b7fc50d
Daria Kurmaeva <d.kurmaeva.speople@gmail.com>
2018-08-19 14:38:58 +0600

    Add What people say block

--- dist/scripts/index.js
+++ dist/scripts/index.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", (event) => {
   new NavBar();
+  new Reviews('reviews_1');
 });
 
 class NavBar {
@@ -26,5 +27,39 @@ class NavBar {
     toggleIcon.classList.toggle('fa-bars');
     toggleIcon.classList.toggle('fa-times');
     this.menu.classList.toggle('opened');
+  }
+}
+
+class Reviews {
+  constructor(id) {
+    this.onClick = this.onClick.bind(this);
+
+    this.reviewsList = Array.from(document.querySelector(`#${id} .reviews-list`).children);
+    this.paginationPages = Array.from(document.querySelector(`#${id} .pagination .pages`).children);
+    this.leftArrow = document.querySelector(`#${id} .left-arrow`);
+    this.rightArrow = document.querySelector(`#${id} .right-arrow`);
+
+    this.leftArrow.addEventListener('click', this.onClick);
+    this.rightArrow.addEventListener('click', this.onClick);
+  }
+
+  onClick(event) {
+    const step = (event.target.dataset.dataArrow == 'left') ? -1 : 1;
+    const pageCount = this.paginationPages.length;
+    let pageChangeStatus = false;
+
+    this.paginationPages.forEach((pageNumber, index) => {
+      if (pageNumber.classList.contains('active') && !pageChangeStatus) {
+        pageNumber.classList.remove('active');
+        this.reviewsList[index].classList.remove('active');
+
+        let nextIndex = index + step;
+        nextIndex = (nextIndex < 0) ? pageCount - 1 : (nextIndex == pageCount) ? 0 : nextIndex;
+
+        this.paginationPages[nextIndex].classList.add('active');
+        this.reviewsList[nextIndex].classList.add('active');
+        pageChangeStatus = true;
+      }
+    });
   }
 }
