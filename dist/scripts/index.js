aafc5960db346e6bd6e6419a83ad1deb236686c7
Daria Kurmaeva <d.kurmaeva.speople@gmail.com>
2018-08-27 11:56:24 +0600

    Add LangMenu to Header

--- dist/scripts/index.js
+++ dist/scripts/index.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
   new NavBar();
   new Reviews('reviews_1');
   new Pagination('reviews_2');
+  new Dropdown('lang-menu');
 });
 
 class NavBar {
@@ -102,5 +103,27 @@ class Pagination {
         }
       }
     });
+  }
+}
+
+class Dropdown {
+  constructor(id) {
+    this.dropdown = document.querySelector(`#${id}`);
+    this.toggle = document.querySelector(`#${id} .dropdown-toggle`);
+    this.backdrop = document.querySelector(`#${id} .dropdown-backdrop`);
+
+    this.onToggleClick = this.onToggleClick.bind(this);
+    this.onBackdropClick = this.onBackdropClick.bind(this);
+
+    this.toggle.addEventListener('click', this.onToggleClick);
+    this.backdrop.addEventListener('click', this.onBackdropClick);
+  }
+
+  onToggleClick() {
+    this.dropdown.classList.add('opened');
+  }
+
+  onBackdropClick() {
+    this.dropdown.classList.remove('opened');
   }
 }
