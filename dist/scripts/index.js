ba9dfa0dfc8e8624e5590429a251e53ef64e0523
Daria Kurmaeva <d.kurmaeva.speople@gmail.com>
2018-08-31 13:49:00 +0600

    Add Get In Touch block to Home page

--- dist/scripts/index.js
+++ dist/scripts/index.js
@@ -4,6 +4,9 @@ document.addEventListener("DOMContentLoaded", (event) => {
   new Dropdown('currency-menu');
   new Dropdown('lang-menu-mobile');
   new Dropdown('currency-menu-mobile');
+  new TextFielsdSetup(document.querySelectorAll('textarea'));
+  new TextFielsdSetup(document.querySelectorAll('input[type="text"]'));
+  new Contacts();
 });
 
 class NavBar {
@@ -52,5 +55,32 @@ class Dropdown {
 
   onBackdropClick() {
     this.dropdown.classList.remove('opened');
+  }
+}
+
+class TextFielsdSetup {
+  constructor(textFileds) {
+    this.textFileds = Array.from(textFileds);
+    this.onKeyUp = this.onKeyUp.bind(this);
+    this.textFileds.forEach(textFiled => textFiled.addEventListener('keyup', this.onKeyUp));
+  }
+
+  onKeyUp(event) {
+    (event.target.value) ? event.target.classList.add('dirty') : event.target.classList.remove('dirty');
+  }
+}
+
+class Contacts {
+  constructor() {
+    this.contacts = Array.from(document.querySelectorAll('.contacts .body'));
+    this.toggles = Array.from(document.querySelectorAll('.contacts .body .button'));
+
+    this.onClick = this.onClick.bind(this);
+
+    this.toggles.forEach(toggle => toggle.addEventListener('click', this.onClick));
+  }
+
+  onClick(event) {
+    this.contacts.forEach(body => body.classList.toggle('show'));
   }
 }
